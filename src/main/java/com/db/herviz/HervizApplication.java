2a9fff11fc970edd141ba053f773bd6a4c87cac8
chenweiqi98 <chenweiqi98@gmail.com>
2022-04-17 17:29:37 -0400

    MP retrieving data demo Customer service mapper entity structure finished

--- src/main/java/com/db/herviz/HervizApplication.java
+++ src/main/java/com/db/herviz/HervizApplication.java
@@ -1,13 +1,24 @@
 package com.db.herviz;
 
+import com.db.herviz.service.CustomerService;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 public class HervizApplication {
+
+    @Resource
+    private CustomerService customerService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(HervizApplication.class, args);
+
     }
 
 }
---
+++ src/main/java/com/db/herviz/entity/Customer.java
@@ -0,0 +1,40 @@
+package com.db.herviz.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @Author: Chen Weiqi
+ * @Date: 2022/4/17 16:35
+ */
+@Data
+@TableName("zcq_customer")
+public class Customer {
+
+    @TableId("c_id")
+    private Integer id;
+
+    @TableField("c_type")
+    private Boolean type;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Integer phoneNo;
+
+    private String address;
+
+    private String country;
+
+    private String state;
+
+    private String city;
+
+    private String zipCode;
+
+}
