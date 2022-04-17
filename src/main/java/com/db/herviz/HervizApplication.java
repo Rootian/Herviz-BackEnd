ea574df948514c3a9472f8e3f774b1d9c9f31fa7
= <846587073@qq.com>
2022-04-17 14:48:11 -0400

    create proj

---
+++ src/main/java/com/db/herviz/HervizApplication.java
@@ -0,0 +1,13 @@
+package com.db.herviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HervizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HervizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/db/herviz/HervizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.herviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HervizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
