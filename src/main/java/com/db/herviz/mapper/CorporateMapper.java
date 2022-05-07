6509e61728357b1f9e8adc965806b8d894a7d3db
= <846587073@qq.com>
2022-05-07 19:13:41 -0400

    add corporate list

---
+++ src/main/java/com/db/herviz/mapper/CorporateMapper.java
@@ -0,0 +1,9 @@
+package com.db.herviz.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.db.herviz.entity.Corporate;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CorporateMapper extends BaseMapper<Corporate> {
+}
